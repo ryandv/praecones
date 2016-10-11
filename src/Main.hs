68837e54cdde49067d6a054dbe00293f3a4028ac
Ryan DV <rdevilla@uwaterloo.ca>
2016-10-11 00:54:24 -0400

    Implement mpsc with QSem.

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.Chan
-import Control.Concurrent.MVar
+import Control.Concurrent.QSem
 
 import Control.Exception
 
@@ -54,10 +54,10 @@ timeFormat :: String
 timeFormat = "%Y %m %d %X"
 
 maxLen :: Int
-maxLen = 1
+maxLen = 2
 
 logMessage :: String -> IO ()
-logMessage msg = do
+logMessage msg = (flip catch) ((\e -> return ()) :: SomeException -> IO ()) $ do
   withFile "/tmp/praecones.log" AppendMode $ \h -> do
     hSetBuffering h NoBuffering
     hPutStrLn h msg
@@ -80,9 +80,8 @@ main = do
       hSetBuffering stdout NoBuffering
 
       -- Initialize shared state.
-      producerWakeup <- newEmptyMVar
-      consumerWakeup <- newEmptyMVar
-      eventQueueLen <- newIORef 0
+      producerSemaphore <- newQSem maxLen
+      consumerSemaphore <- newQSem 0
       eventQueue <- fmap EventQueue $ newChan
 
       -- Initialize thread-local mutable statusbar ref.
@@ -96,25 +95,25 @@ main = do
       hSetBuffering dzen2Stdin' NoBuffering
 
       -- Create tick threads.
-      tickTimeThread <- forkIO . forever $ systemTimeTicker producerWakeup consumerWakeup eventQueueLen eventQueue
-      readXmonadUpdatesThread <- forkIO . forever $ xmonadUpdateReader producerWakeup consumerWakeup eventQueueLen eventQueue
+      tickTimeThread <- forkIO . forever $ systemTimeTicker producerSemaphore consumerSemaphore eventQueue
+      readXmonadUpdatesThread <- forkIO . forever $ xmonadUpdateReader producerSemaphore consumerSemaphore eventQueue
 
       -- Print to dzen2.
-      forever $ printCurrentStatusbar producerWakeup consumerWakeup eventQueueLen dzen2Stdin' statusbar eventQueue
+      forever $ printCurrentStatusbar producerSemaphore consumerSemaphore dzen2Stdin' statusbar eventQueue
 
-printCurrentStatusbar :: MVar () -> MVar () -> IORef Int -> Handle -> IORef Statusbar -> EventQueue StatusbarUpdateEvent -> IO ()
-printCurrentStatusbar producerWakeup consumerWakeup lenref h ioref (EventQueue chan) = do
+printCurrentStatusbar :: QSem -> QSem -> Handle -> IORef Statusbar -> EventQueue StatusbarUpdateEvent -> IO ()
+printCurrentStatusbar producerSemaphore consumerSemaphore h ioref (EventQueue chan) = do
     currentStatusbar <- readIORef ioref
 
-    nextEvent <- consumer producerWakeup consumerWakeup lenref (EventQueue chan)
+    nextEvent <- consumer producerSemaphore consumerSemaphore (EventQueue chan)
     let nextStatusbar = applyStatusbarUpdate currentStatusbar nextEvent
 
     writeIORef ioref nextStatusbar
 
     liftIO . hPutStrLn h $ "^fg(#e4e4e4)^pa(0)" ++ (xmonadSection nextStatusbar) ++ " ^pa(1500)" ++ (datetimeSection nextStatusbar)
 
-systemTimeTicker :: MVar () -> MVar () -> IORef Int -> EventQueue StatusbarUpdateEvent -> IO ()
-systemTimeTicker producerWakeup consumerWakeup lenref (EventQueue chan) = do
+systemTimeTicker :: QSem -> QSem -> EventQueue StatusbarUpdateEvent -> IO ()
+systemTimeTicker producerSemaphore consumerSemaphore (EventQueue chan) = do
     curTime <- getCurrentTime
     localZonedTime <- utcToLocalZonedTime curTime
 
@@ -123,43 +122,34 @@ systemTimeTicker producerWakeup consumerWakeup lenref (EventQueue chan) = do
 
     let statusbarUpdate = StatusbarUpdateEvent Nothing (Just formattedTime)
 
-    producer "TIME" producerWakeup consumerWakeup lenref (EventQueue chan) statusbarUpdate
+    producer "TIME" producerSemaphore consumerSemaphore (EventQueue chan) statusbarUpdate
 
-xmonadUpdateReader :: MVar () -> MVar () -> IORef Int -> EventQueue StatusbarUpdateEvent -> IO ()
-xmonadUpdateReader producerWakeup consumerWakeup lenref (EventQueue chan) = do
+xmonadUpdateReader :: QSem -> QSem -> EventQueue StatusbarUpdateEvent -> IO ()
+xmonadUpdateReader producerSemaphore consumerSemaphore (EventQueue chan) = do
     nextStatus <- getLine
     logMessage "xmonad update received"
 
     let statusbarUpdate = StatusbarUpdateEvent (Just nextStatus) Nothing
 
-    producer "XMONAD" producerWakeup consumerWakeup lenref (EventQueue chan) statusbarUpdate
+    producer "XMONAD" producerSemaphore consumerSemaphore (EventQueue chan) statusbarUpdate
 
-consumer :: MVar () -> MVar () -> IORef Int -> EventQueue StatusbarUpdateEvent -> IO StatusbarUpdateEvent
-consumer producerWakeup consumerWakeup lenref (EventQueue chan) = do
-    len <- readIORef lenref
+consumer :: QSem -> QSem -> EventQueue StatusbarUpdateEvent -> IO StatusbarUpdateEvent
+consumer producerSemaphore consumerSemaphore (EventQueue chan) = do
+    logMessage "consumer waiting on semaphore"
+    waitQSem consumerSemaphore
 
-    when (len == 0) $ do
-      logMessage "consumer waiting"
-      takeMVar consumerWakeup
+    logMessage "consumer proceeding"
 
-    newLen <- readIORef lenref;                                     logMessage $ "consumer proceeding with length " ++ (show newLen)
-    nextEvent <- readChan chan;                                     logMessage "consumer reading from chan"
-
-    writeIORef lenref (newLen - 1)                               >> logMessage "consumer decreasing count"
-    when (newLen == maxLen) $ putMVar producerWakeup ()          >> logMessage "waking up producer"
+    nextEvent <- readChan chan;                                        logMessage "consumer reading from chan"
+    signalQSem producerSemaphore                                    >> logMessage "consumer increasing producer semaphore"
     return nextEvent
 
-producer :: String -> MVar () -> MVar () -> IORef Int -> EventQueue StatusbarUpdateEvent -> StatusbarUpdateEvent -> IO ()
-producer name producerWakeup consumerWakeup lenref (EventQueue chan) statusbarUpdate = do
-    len <- readIORef lenref
+producer :: String -> QSem -> QSem -> EventQueue StatusbarUpdateEvent -> StatusbarUpdateEvent -> IO ()
+producer name producerSemaphore consumerSemaphore (EventQueue chan) statusbarUpdate = do
+    logMessage $ name ++ " waiting on semaphore"
+    waitQSem producerSemaphore
 
-    when (len == maxLen) $ do
-      logMessage $ name ++ " waiting"
-      takeMVar producerWakeup
-
-    newLen <- readIORef lenref;                                     logMessage $ name ++ " proceeding with length " ++ (show newLen)
+    logMessage $ name ++ " proceeding"
 
     writeChan chan statusbarUpdate                               >> (logMessage $ name ++ " writing to chan")
-    writeIORef lenref (newLen + 1)                               >> (logMessage $ name ++ " increasing count")
-
-    when (newLen == 0) $  putMVar consumerWakeup ()              >> logMessage "waking up consumer"
+    signalQSem consumerSemaphore                                 >> (logMessage $ name ++ " increasing consumer semaphore")
