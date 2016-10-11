98a1688555cf0d17b2a88b758781e90fd2dc3544
Ryan DV <rdevilla@uwaterloo.ca>
2016-10-10 20:35:22 -0400

    Attempted implementation of bounded channel with logging.

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,8 @@ import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.Chan
 import Control.Concurrent.MVar
 
+import Control.Exception
+
 import Control.Monad
 import Control.Monad.IO.Class
 import Control.Monad.State.Strict
@@ -51,7 +53,17 @@ dzen2Spec = CreateProcess
 timeFormat :: String
 timeFormat = "%Y %m %d %X"
 
-{-- END CONFIG --}
+maxLen :: Int
+maxLen = 1
+
+logMessage :: String -> IO ()
+logMessage msg = do
+  withFile "/tmp/praecones.log" AppendMode $ \h -> do
+    hSetBuffering h NoBuffering
+    hPutStrLn h msg
+
+logException :: SomeException -> IO ()
+logException ex = logMessage (show ex)
 
 applyStatusbarUpdate :: Statusbar -> StatusbarUpdateEvent -> Statusbar
 applyStatusbarUpdate si ev = Statusbar nextXmonadSection nextDatetimeSection
@@ -59,8 +71,50 @@ applyStatusbarUpdate si ev = Statusbar nextXmonadSection nextDatetimeSection
     nextXmonadSection = fromMaybe (xmonadSection si) (newXmonadSection ev)
     nextDatetimeSection = fromMaybe (datetimeSection si) (newDatetimeSection ev)
 
-systemTimeTicker :: EventQueue StatusbarUpdateEvent -> IO ()
-systemTimeTicker (EventQueue chan) = do
+{-- END CONFIG --}
+
+main :: IO ()
+main = do
+    (flip catch) logException $ do
+      -- Print to stdout on demand
+      hSetBuffering stdout NoBuffering
+
+      -- Initialize shared state.
+      producerWakeup <- newEmptyMVar
+      consumerWakeup <- newEmptyMVar
+      eventQueueLen <- newIORef 0
+      eventQueue <- fmap EventQueue $ newChan
+
+      -- Initialize thread-local mutable statusbar ref.
+      statusbar <- newIORef $ Statusbar "" ""
+
+      -- Initialize dzen2 and hook up to its STDIN
+      (dzen2Stdin, dzen2Stdout, dzen2Stderr, dzen2Ph) <- createProcess dzen2Spec
+
+      -- Assume we can acquire a handle for now...
+      let dzen2Stdin' = fromJust $ dzen2Stdin
+      hSetBuffering dzen2Stdin' NoBuffering
+
+      -- Create tick threads.
+      tickTimeThread <- forkIO . forever $ systemTimeTicker producerWakeup consumerWakeup eventQueueLen eventQueue
+      readXmonadUpdatesThread <- forkIO . forever $ xmonadUpdateReader producerWakeup consumerWakeup eventQueueLen eventQueue
+
+      -- Print to dzen2.
+      forever $ printCurrentStatusbar producerWakeup consumerWakeup eventQueueLen dzen2Stdin' statusbar eventQueue
+
+printCurrentStatusbar :: MVar () -> MVar () -> IORef Int -> Handle -> IORef Statusbar -> EventQueue StatusbarUpdateEvent -> IO ()
+printCurrentStatusbar producerWakeup consumerWakeup lenref h ioref (EventQueue chan) = do
+    currentStatusbar <- readIORef ioref
+
+    nextEvent <- consumer producerWakeup consumerWakeup lenref (EventQueue chan)
+    let nextStatusbar = applyStatusbarUpdate currentStatusbar nextEvent
+
+    writeIORef ioref nextStatusbar
+
+    liftIO . hPutStrLn h $ "^fg(#e4e4e4)^pa(0)" ++ (xmonadSection nextStatusbar) ++ " ^pa(1500)" ++ (datetimeSection nextStatusbar)
+
+systemTimeTicker :: MVar () -> MVar () -> IORef Int -> EventQueue StatusbarUpdateEvent -> IO ()
+systemTimeTicker producerWakeup consumerWakeup lenref (EventQueue chan) = do
     curTime <- getCurrentTime
     localZonedTime <- utcToLocalZonedTime curTime
 
@@ -68,49 +122,44 @@ systemTimeTicker (EventQueue chan) = do
     let formattedTime = formatTime defaultTimeLocale timeFormat localCurTime
 
     let statusbarUpdate = StatusbarUpdateEvent Nothing (Just formattedTime)
-    writeChan chan statusbarUpdate
 
-    threadDelay 1000000 -- one second
+    producer "TIME" producerWakeup consumerWakeup lenref (EventQueue chan) statusbarUpdate
 
-xmonadUpdateReader :: EventQueue StatusbarUpdateEvent -> IO ()
-xmonadUpdateReader (EventQueue chan) = do
+xmonadUpdateReader :: MVar () -> MVar () -> IORef Int -> EventQueue StatusbarUpdateEvent -> IO ()
+xmonadUpdateReader producerWakeup consumerWakeup lenref (EventQueue chan) = do
     nextStatus <- getLine
+    logMessage "xmonad update received"
 
-    writeChan chan $ StatusbarUpdateEvent (Just nextStatus) Nothing
+    let statusbarUpdate = StatusbarUpdateEvent (Just nextStatus) Nothing
 
-printCurrentStatusbar :: Handle -> IORef Statusbar -> EventQueue StatusbarUpdateEvent -> IO ()
-printCurrentStatusbar h ioref (EventQueue chan) = do
-    currentStatusbar <- readIORef ioref
+    producer "XMONAD" producerWakeup consumerWakeup lenref (EventQueue chan) statusbarUpdate
 
-    nextStatusbar <- do
-        nextEvent <- readChan chan
-        return $ applyStatusbarUpdate currentStatusbar nextEvent
+consumer :: MVar () -> MVar () -> IORef Int -> EventQueue StatusbarUpdateEvent -> IO StatusbarUpdateEvent
+consumer producerWakeup consumerWakeup lenref (EventQueue chan) = do
+    len <- readIORef lenref
 
-    writeIORef ioref nextStatusbar
+    when (len == 0) $ do
+      logMessage "consumer waiting"
+      takeMVar consumerWakeup
 
-    liftIO . hPutStrLn h $ "^fg(#e4e4e4)^pa(0)" ++ (xmonadSection nextStatusbar) ++ " ^pa(1500)" ++ (datetimeSection nextStatusbar)
+    newLen <- readIORef lenref;                                     logMessage $ "consumer proceeding with length " ++ (show newLen)
+    nextEvent <- readChan chan;                                     logMessage "consumer reading from chan"
 
-main :: IO ()
-main = do
-    -- Print to stdout on demand
-    hSetBuffering stdout NoBuffering
+    writeIORef lenref (newLen - 1)                               >> logMessage "consumer decreasing count"
+    when (newLen == maxLen) $ putMVar producerWakeup ()          >> logMessage "waking up producer"
+    return nextEvent
 
-    -- Initialize shared state.
-    eventQueue <- fmap EventQueue $ newChan
+producer :: String -> MVar () -> MVar () -> IORef Int -> EventQueue StatusbarUpdateEvent -> StatusbarUpdateEvent -> IO ()
+producer name producerWakeup consumerWakeup lenref (EventQueue chan) statusbarUpdate = do
+    len <- readIORef lenref
 
-    -- Initialize thread-local mutable statusbar ref.
-    statusbar <- newIORef $ Statusbar "" ""
+    when (len == maxLen) $ do
+      logMessage $ name ++ " waiting"
+      takeMVar producerWakeup
 
-    -- Initialize dzen2 and hook up to its STDIN
-    (dzen2Stdin, dzen2Stdout, dzen2Stderr, dzen2Ph) <- createProcess dzen2Spec
+    newLen <- readIORef lenref;                                     logMessage $ name ++ " proceeding with length " ++ (show newLen)
 
-    -- Assume we can acquire a handle for now...
-    let dzen2Stdin' = fromJust $ dzen2Stdin
-    hSetBuffering dzen2Stdin' NoBuffering
+    writeChan chan statusbarUpdate                               >> (logMessage $ name ++ " writing to chan")
+    writeIORef lenref (newLen + 1)                               >> (logMessage $ name ++ " increasing count")
 
-    -- Create tick threads.
-    tickTimeThread <- forkIO . forever $ systemTimeTicker eventQueue
-    readXmonadUpdatesThread <- forkIO . forever $ xmonadUpdateReader eventQueue
-
-    -- Print to dzen2.
-    forever $ printCurrentStatusbar dzen2Stdin' statusbar eventQueue
+    when (newLen == 0) $  putMVar consumerWakeup ()              >> logMessage "waking up consumer"
